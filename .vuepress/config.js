550103e820f71869dc20b05723fde71c961fc3b0
Luterngun <47859358+Luterngun@users.noreply.github.com>
2021-02-26 15:28:30 +0000

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   base:'/',
   locales: {
-    '/docs/': {
+    '/': {
       lang: 'zh-hk',
       title: '數字極權時代生存手記'
     },
-    '/docs/zh/': {
+    '/zh/': {
       lang: 'zh-CN',
       title: '数字极权时代生存手记'
     },
@@ -13,80 +13,80 @@ module.exports = {
   themeConfig: {
     smoothScroll: true,
     locales: {
-      '/docs/': {
+      '/': {
         selectText: '选择语言',
         label: '繁體中文',
         ariaLabel: '选择语言',
         algolia: {},
         nav: [
-          { text: '主頁',  link: '/docs/' },
-          { text: '正文', link: '/docs/book/' }
+          { text: '主頁',  link: '/' },
+          { text: '正文', link: '/book/' }
         ],
         sidebar:[
         {
           title: '目錄',
-          path: '/docs/book/',
+          path: '/book/',
           collapsable: false,
           sidebarDepth: 1,
           displayAllHeaders: true,
           children: [
-            '/docs/book/0-1.md',
-            '/docs/book/0-2.md',
-            '/docs/book/1.md',
-            '/docs/book/2.md',
-            '/docs/book/3.md',
-            '/docs/book/4.md',
-            '/docs/book/5.md',
-            '/docs/book/6.md',
-            '/docs/book/7.md',
-            '/docs/book/8.md',
-            '/docs/book/9.md',
-            '/docs/book/10.md',
-            '/docs/book/11.md',
-            '/docs/book/12.md',
-            '/docs/book/13.md',
-            '/docs/book/14.md',
-            '/docs/book/15.md',
-            '/docs/book/ap.md',
+            '/book/0-1.md',
+            '/book/0-2.md',
+            '/book/1.md',
+            '/book/2.md',
+            '/book/3.md',
+            '/book/4.md',
+            '/book/5.md',
+            '/book/6.md',
+            '/book/7.md',
+            '/book/8.md',
+            '/book/9.md',
+            '/book/10.md',
+            '/book/11.md',
+            '/book/12.md',
+            '/book/13.md',
+            '/book/14.md',
+            '/book/15.md',
+            '/book/ap.md',
           ]
         }
         ]
       },
-      '/docs/zh/': {
+      '/zh/': {
         selectText: '選擇語言',
         label: '简体中文',
         ariaLabel: '選擇語言',
         algolia: {},
         nav: [
-          { text: '主页',  link: '/docs/zh/' },
-          { text: '正文', link: '/docs/zh/book/' }
+          { text: '主页',  link: '/zh/' },
+          { text: '正文', link: '/zh/book/' }
         ],
         sidebar:[
         {
           title: '目录',
-          path: '/docs/zh/book/',
+          path: '/zh/book/',
           collapsable: false,
           sidebarDepth: 1,
           displayAllHeaders: true,
           children: [
-            '/docs/zh/book/0-1.md',
-            '/docs/zh/book/0-2.md',
-            '/docs/zh/book/1.md',
-            '/docs/zh/book/2.md',
-            '/docs/zh/book/3.md',
-            '/docs/zh/book/4.md',
-            '/docs/zh/book/5.md',
-            '/docs/zh/book/6.md',
-            '/docs/zh/book/7.md',
-            '/docs/zh/book/8.md',
-            '/docs/zh/book/9.md',
-            '/docs/zh/book/10.md',
-            '/docs/zh/book/11.md',
-            '/docs/zh/book/12.md',
-            '/docs/zh/book/13.md',
-            '/docs/zh/book/14.md',
-            '/docs/zh/book/15.md',
-            '/docs/zh/book/ap.md',
+            '/zh/book/0-1.md',
+            '/zh/book/0-2.md',
+            '/zh/book/1.md',
+            '/zh/book/2.md',
+            '/zh/book/3.md',
+            '/zh/book/4.md',
+            '/zh/book/5.md',
+            '/zh/book/6.md',
+            '/zh/book/7.md',
+            '/zh/book/8.md',
+            '/zh/book/9.md',
+            '/zh/book/10.md',
+            '/zh/book/11.md',
+            '/zh/book/12.md',
+            '/zh/book/13.md',
+            '/zh/book/14.md',
+            '/zh/book/15.md',
+            '/zh/book/ap.md',
           ]
         },
         ]
@@ -96,7 +96,7 @@ module.exports = {
     repo: 'reconsidera/reconsidera.github.io',
     repoLabel: '查看原碼',
     docsRepo: 'reconsidera/reconsidera.github.io',
-    docsDir: '/docs/',
+    docsDir: '/',
     docsBranch: 'main'
   }
 }
